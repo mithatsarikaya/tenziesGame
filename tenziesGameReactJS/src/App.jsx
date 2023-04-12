f1bd9ecb220190112f06ef7901969016c1ad5a64
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-04-12 12:51:30 +0300

    will be added: style, little bit added

--- tenziesGameReactJS/src/App.jsx
+++ tenziesGameReactJS/src/App.jsx
@@ -64,7 +64,8 @@ export default function App(){
     return(
         <main>
             {tenzies && <Confetti />}
-            <div className="gameDesc">Tenzies game</div>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dicesContainer">
                 {diceElements}
             </div>
