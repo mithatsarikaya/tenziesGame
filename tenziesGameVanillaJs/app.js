e92c852fa4293acd51aac0e29075a3543d65253f
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-04-12 20:25:17 +0300

    done: vanillaJs has no confetti feature like reactJs version :(

--- tenziesGameVanillaJs/app.js
+++ tenziesGameVanillaJs/app.js
@@ -4,13 +4,37 @@ function generateDiceNumber() {
 }
 let diceDivs = document.querySelectorAll(".dice--face");
 
+function checkIfTenzie() {
+  let classListOfDivs = [];
+  diceDivs.forEach((d) => {
+    classListOfDivs.push({
+      value: d.innerText,
+      isHeld: Object.values(d.classList).includes("clicked"),
+    });
+  });
+
+  //check if they all held
+  let areAllHeld = classListOfDivs.every((c) => c.isHeld === true);
+  //get one value from dices
+  let oneValueFromGame = classListOfDivs[0].value;
+  //check if they all same value
+  let areAllSame = classListOfDivs.every((c) => c.value === oneValueFromGame);
+
+  //if winning conditions are okey, then ask user for a new game
+  if (areAllHeld && areAllSame) {
+    if (confirm("Do you want to play new game?")) {
+      window.location.reload();
+    }
+  }
+}
+
 //in order held dice add them the new class "clicked"
 //if they have it remove it if they have not then add it
 diceDivs.forEach((d) => {
   d.addEventListener("click", (e) => {
     let isClicked = !Object.values(e.target.classList).includes("clicked");
-    console.log(isClicked);
     e.target.classList.toggle("clicked", isClicked);
+    checkIfTenzie();
   });
 });
 
@@ -24,11 +48,15 @@ function rollDices() {
 }
 
 rollDices();
+checkIfTenzie();
 
 const rollBtn = document.querySelector(".roll-dice");
 
 //rollBtn
-rollBtn.addEventListener("click", () => rollDices());
+rollBtn.addEventListener("click", () => {
+  rollDices();
+  checkIfTenzie();
+});
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "b") {
